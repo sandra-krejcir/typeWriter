83f9a3211b8ceb299f5c9d8eb9eabc8040481324
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-08-31 18:05:43 +0200

    the letter by letter works

--- typewriter.js
+++ typewriter.js
@@ -1,20 +1,26 @@
 "use strict";
-let maxNumberOfIterations;
-let iterartor;
+let maxIterations;
+let iterator;
+const theText = document.querySelector(".typewritten").textContent.trim();
 
 init();
 
 function init() {
-  maxNumberOfIterations = 11;
+  console.log("function init()");
+  maxIterations = theText.length + 1;
   iterator = 0;
+
+  document.querySelector("#typewriter").innerHTML = "";
   loop();
 }
 
 function loop() {
   console.log("function loop()", iterator);
+  console.log("theText[iterator]", theText[iterator]);
   iterator++;
 
-  if (iterator < maxNumberOfIterations) {
-    setTimeout(loop, 500);
+  if (iterator < maxIterations) {
+    document.querySelector("#typewriter").textContent += theText[iterator - 1];
+    setTimeout(loop, 200);
   }
 }
