9f4c640b0ed558c03837809c9d62fc25cc10974b
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-10-19 16:34:10 +0200

    added the line

--- typewriter.js
+++ typewriter.js
@@ -18,9 +18,11 @@ function loop() {
   console.log("function loop()", iterator);
   console.log("theText[iterator]", theText[iterator]);
   iterator++;
+  document.querySelector("#theWriter").classList.add("hide");
 
   if (iterator < maxIterations) {
     document.querySelector("#typewriter").textContent += theText[iterator - 1];
+    document.querySelector("#theWriter").classList.remove("hide");
     setTimeout(loop, 200);
   }
 }
